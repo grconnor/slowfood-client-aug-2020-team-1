4409db2af0a846e263a1eabb15417b62ed3a58d2
oliverochman <oliver.ochman@hotmail.com>
2020-09-22 11:35:23 +0200

    add Menu component and products module that makes request to api

---
+++ src/components/Menu.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { getProducts } from '../modules/products'
+
+class Menu extends Component {
+  state = {
+    menu: []
+  }
+  
+  componentDidMount = async () => {
+    const products = await getProducts()
+    this.setState({menu: products})
+  }
+
+  render() {
+    const menu = this.state.menu;
+    return (
+      <>
+        { menu.length > 0 && menu.map(item => {
+          return (
+            <>
+              <div data-cy={"product-" + item.id} id={"product-" + item.id}>
+                <p>{item.name}</p>
+                <p>{item.price}</p>
+              </div>
+            </>
+          );
+        })}
+      </>
+    )
+  }
+}
+
+export default Menu
