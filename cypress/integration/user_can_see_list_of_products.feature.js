50f33c295eb7ce4e1b0a8156010c983a27b90d73
munemo <john.chimbani@outlook.com>
2020-09-22 20:49:23 +0200

    Creating menu.jsx and user_can_see_list_of_products test file.

---
+++ cypress/integration/user_can_see_list_of_products.feature.js
@@ -0,0 +1,23 @@
+describe("User can see list of products", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3001");
+    cy.get("#menu-tab").click();
+  });
+  it("displays first product", () => {
+    cy.get("#product-1").within(() => {
+      cy.get("").should("exist");
+      cy.get(".ui.header").should("contain", "Pizza");
+      cy.get(".description").should(
+        "contain",
+        "Pizza"
+      );
+    });
+  });
+  it("displays secound product", () => {
+    cy.get("#product-2").within(() => {
+      cy.get("").should("exist");
+      cy.get(".ui.header").should("contain", "Korv");
+      cy.get(".description").should("contain", "Korv");
+    });
+  });
+})
