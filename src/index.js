863bbbbb72d5270872aa0a5015cb3fa32c85bc94
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-09-23 12:09:28 +0200

    Deleting unnecessary folders

--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Header from "./Header";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
-import Products from "./Products";
-import Welcome from "./Welcome";
+import App from './App'
 
-const App = () => {
-  return (
-  <>
-    <Header />
-    <Switch>
-      <Route exact path="/" component={Welcome}></Route>
-      <Route exact path="/products" component={Products}></Route>
-    </Switch>
-  </>
-  );
-};
+
 
 ReactDOM.render(
-<BrowserRouter>
-  <Header />
-  <Products />
-</BrowserRouter>,
-document.getElementById("app")
+    <App />,
+  document.getElementById("root")
 );
-
