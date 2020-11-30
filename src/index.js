fe8584a689412f3f5345360a9fb6aa0ef85daf99
Connor Roelofsen <connorroelofsen15@gmail.com>
2020-11-30 18:56:43 +0100

    Fix apiurl declaration.

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ axios.defaults.baseURL = 'http://localhost:3000/api/v1';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 if (process.env.NODE_ENV === "production") {
-  apiUrl = "https://sl-slowfood-api.herokuapp.com/api/v1";
+  var apiUrl = "https://sl-slowfood-api.herokuapp.com/api/v1";
 } else {
-  apiUrl = "http://localhost:3000/api/v1";
+  var apiUrl = "http://localhost:3000/api/v1";
 }
 axios.defaults.baseURL = apiUrl;
 
